2dfcfaa723461eabad04c621c579a8694929f9de
emil.gerth <emil.gerth@shiftwise.com>
2017-07-13 14:59:33 -0700

    added feature 4

---
+++ revert/feature-4.sql
@@ -0,0 +1,7 @@
+-- Revert sandbox:feature-4 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ verify/feature-4.sql
@@ -0,0 +1,7 @@
+-- Verify sandbox:feature-4 on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
